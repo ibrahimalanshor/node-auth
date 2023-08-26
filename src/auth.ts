dece947563a36b22c5a1f75f84efbb6705eb95ab
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2023-08-26 16:23:09 +0700

    feat: lint

--- src/auth.ts
+++ src/auth.ts
@@ -1,26 +1,26 @@
-import { AuthError } from "./errors/auth.error"
+import { AuthError } from './errors/auth.error';
 
 export interface RegisterCredential {
-    email: string
-    password: string
+  email: string;
+  password: string;
 }
 
 export abstract class Auth {
-    protected abstract createUser(credential: RegisterCredential): Promise<any>
+  protected abstract createUser(credential: RegisterCredential): Promise<any>;
 
-    async register(credential: RegisterCredential) {
-        try {
-            await this.createUser(credential)
+  async register(credential: RegisterCredential) {
+    try {
+      await this.createUser(credential);
 
-            return new AuthResult
-        } catch (err) {
-            throw new AuthError({
-                name: 'REGISTER_ERROR',
-                message: err instanceof Error ? err.message : 'Craete User Error',
-                cause: err
-            })
-        }
+      return new AuthResult();
+    } catch (err) {
+      throw new AuthError({
+        name: 'REGISTER_ERROR',
+        message: err instanceof Error ? err.message : 'Craete User Error',
+        cause: err,
+      });
     }
+  }
 }
 
 export class AuthResult {}
